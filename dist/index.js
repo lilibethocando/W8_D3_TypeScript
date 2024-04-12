2c5eb58968e6cb822efb5dd8365f890d783bc7bb
Lilibeth <lilibethocando@gmail.com>
2024-04-12 08:13:08 -0500

    Homeword day 4

--- dist/index.js
+++ dist/index.js
@@ -1,103 +1,150 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const uuid_1 = require("uuid");
-function createUser(name, age) {
-    const id = (0, uuid_1.v4)();
-    const userName = name;
-    const userAge = age;
-    let cart = [];
-    return {
-        id,
-        name: userName,
-        age: userAge,
-        cart
-    };
-}
-function createItem(name, price, description) {
-    const id = (0, uuid_1.v4)();
-    const itemName = name;
-    const itemPrice = price;
-    const itemDescription = description;
-    return {
-        id,
-        name: itemName,
-        price: itemPrice,
-        description: itemDescription
-    };
-}
-function addToCart(user, item) {
-    user.cart.push(item);
-}
-function removeFromCart(user, item) {
-    if (user.cart.includes(item)) {
-        user.cart = user.cart.filter(cartItem => cartItem !== item);
+class Item {
+    constructor(name, price, description) {
+        this._id = (0, uuid_1.v4)();
+        this._name = name;
+        this._price = price;
+        this._description = description;
     }
-    else {
-        console.log(`Item ${item.name} not in the cart!`);
+    get id() {
+        return this._id;
+    }
+    get name() {
+        return this._name;
+    }
+    get price() {
+        return this._price;
+    }
+    get description() {
+        return this._description;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    set price(price) {
+        this._price = price;
+    }
+    set description(description) {
+        this._description = description;
     }
 }
-function removeQuantityFromCart(user, item, quantityToRemove) {
-    let removedQuantity = 0;
-    let itemFound = false;
-    for (let i = 0; i < user.cart.length; i++) {
-        const currentItem = user.cart[i];
-        if (currentItem === item) {
-            itemFound = true;
-            if (removedQuantity < quantityToRemove) {
-                user.cart.splice(i, 1);
-                removedQuantity++;
-                i--;
-            }
-            else {
-                break;
-            }
+class User {
+    constructor(name, age, cart = []) {
+        this._id = (0, uuid_1.v4)();
+        this._name = name;
+        this._age = age;
+        this._cart = cart;
+    }
+    get id() {
+        return this._id;
+    }
+    get name() {
+        return this._name;
+    }
+    get age() {
+        return this._age;
+    }
+    get cart() {
+        return this._cart;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    set age(age) {
+        this._age = age;
+    }
+    set cart(cart) {
+        this._cart = cart;
+    }
+    addToCart(item) {
+        this.cart.push(item);
+    }
+    removeFromCart(item) {
+        if (this.cart.includes(item)) {
+            this.cart = this.cart.filter((cartItem) => cartItem !== item);
+        }
+        else {
+            console.log(`Item ${item.name} not in the cart!`);
         }
     }
-    if (!itemFound) {
-        console.log(`Error: The item (${item.name}) is not in the cart.`);
+    removeQuantityFromCart(item, quantityToRemove) {
+        let removedQuantity = 0;
+        let itemFound = false;
+        for (let i = 0; i < this.cart.length; i++) {
+            const currentItem = this.cart[i];
+            if (currentItem === item) {
+                itemFound = true;
+                if (removedQuantity < quantityToRemove) {
+                    this.cart.splice(i, 1);
+                    removedQuantity++;
+                    i--;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        if (!itemFound) {
+            console.log(`Error: The item (${item.name}) is not in the cart.`);
+        }
+        else if (removedQuantity < quantityToRemove) {
+            console.log(`Error: Quantity to remove (${quantityToRemove}) exceeds the quantity of ${item.name} in the cart.`);
+        }
     }
-    else if (removedQuantity < quantityToRemove) {
-        console.log(`Error: Quantity to remove (${quantityToRemove}) exceeds the quantity of ${item.name} in the cart.`);
+    cartTotal() {
+        let totalPrice = 0;
+        for (let item of this.cart) {
+            totalPrice += item.price;
+        }
+        return totalPrice;
+    }
+    printCart() {
+        console.log("Items in the cart:");
+        for (let item of this.cart) {
+            console.log(item);
+        }
     }
 }
-function cartTotal(user) {
-    let totalPrice = 0;
-    for (let item of user.cart) {
-        totalPrice += item.price;
+class Shop {
+    constructor(name, items = []) {
+        this._name = name;
+        const itemA = new Item("Item A", 10, "Description of Item A");
+        const itemB = new Item("Item B", 20, "Description of Item B");
+        const itemC = new Item("Item C", 15, "Description of Item C");
+        this._items = [itemA, itemB, itemC, ...items];
     }
-    return totalPrice;
-}
-function printCart(user) {
-    for (let item of user.cart) {
-        console.log(item);
+    get name() {
+        return this._name;
     }
-    ;
+    set name(name) {
+        this._name = name;
+    }
+    get items() {
+        return this._items;
+    }
+    set items(items) {
+        this._items = items;
+    }
+    describe() {
+        console.log(`Shop Name: ${this._name}`);
+        console.log("Items in the shop:");
+        this._items.forEach(item => {
+            console.log(`- ${item.name}: $${item.price}`);
+        });
+    }
 }
-const user = createUser("John Doe", 30);
-const itemA = createItem("Item A", 10, "Description of Item A");
-const itemB = createItem("Item B", 20, "Description of Item B");
-const itemC = createItem("Item C", 15, "Description of Item C");
-addToCart(user, itemA);
-printCart(user);
-console.log("Total cart value: $" + cartTotal(user));
-for (let i = 0; i < 3; i++) {
-    addToCart(user, itemB);
-}
-console.log("\nAfter adding 3 Item B to the user's cart:");
-printCart(user);
-console.log("Total cart value: $" + cartTotal(user));
-for (let i = 0; i < 3; i++) {
-    addToCart(user, itemC);
-}
-console.log("\nAfter adding 3 Item C to the user's cart:");
-printCart(user);
-console.log("Total cart value: $" + cartTotal(user));
-removeFromCart(user, itemB);
-console.log("\nAfter removing all of Item B from the user's cart:");
-printCart(user);
-console.log(`Total cart value: $${cartTotal(user)}`);
-removeQuantityFromCart(user, itemC, 2);
-console.log("\nAfter removing 2 of Item C from the user's cart:");
-printCart(user);
-console.log(`Total cart value: $${cartTotal(user)}`);
+const shop = new Shop("My Shop");
+const user = new User("John Doe", 30);
+user.addToCart(shop.items[0]);
+user.addToCart(shop.items[1]);
+user.addToCart(shop.items[1]);
+user.addToCart(shop.items[2]);
+user.printCart();
+user.removeFromCart(shop.items[1]);
+user.printCart();
+user.removeQuantityFromCart(shop.items[2], 1);
+user.printCart();
+console.log("Cart total:", user.cartTotal());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,162 +1,206 @@
 import { v4 as uuidv4 } from "uuid";
 
-// Types
+class Item {
+    private _id: string;
+    private _name: string;
+    private _price: number;
+    private _description: string;
 
-type Item = {
-    readonly id: string,
-    name: string,
-    price: number,
-    description: string
-}
+    constructor(name: string, price: number, description: string) {
+        this._id = uuidv4();
+        this._name = name;
+        this._price = price;
+        this._description = description;
+    }
 
-type User = {
-    readonly id?: string,
-    name: string,
-    age: number,
-    cart: Item[]
-}
+    get id(): string {
+        return this._id;
+    }
 
-function createUser(name:string, age:number): User {
-    const id: string = uuidv4()
-    const userName: string = name
-    const userAge: number = age
-    let cart: Item[] = []
-    return {
-        id,
-        name: userName,
-        age: userAge,
-        cart
-    };
-}
+    get name(): string {
+        return this._name;
+    }
 
+    get price(): number {
+        return this._price;
+    }
 
-function createItem(name:string, price:number, description:string): Item {
-    const id: string = uuidv4()
-    const itemName: string = name
-    const itemPrice: number = price
-    const itemDescription: string = description
-    return {
-        id, 
-        name: itemName,
-        price: itemPrice,
-        description: itemDescription
-    };
-}
+    get description(): string {
+        return this._description;
+    }
 
+    set name(name: string) {
+        this._name = name;
+    }
 
-function addToCart(user:User, item:Item): void {
-    user.cart.push(item);
-}
+    set price(price: number) {
+        this._price = price;
+    }
 
-function removeFromCart(user:User, item:Item): void {
-    if (user.cart.includes(item)) {
-        user.cart = user.cart.filter(cartItem => cartItem !== item);
-    } else {
-        console.log(`Item ${item.name} not in the cart!`)
+    set description(description: string) {
+        this._description = description;
     }
 }
 
+class User {
+    private _id: string;
+    private _name: string;
+    private _age: number;
+    private _cart: Item[];
 
-function removeQuantityFromCart(user: User, item: Item, quantityToRemove: number): void {
-    let removedQuantity = 0;
-    let itemFound = false;
+    constructor(name: string, age: number, cart: Item[] = []) {
+        this._id = uuidv4();
+        this._name = name;
+        this._age = age;
+        this._cart = cart;
+    }
 
-    for (let i = 0; i < user.cart.length; i++) {
-        const currentItem = user.cart[i];
-        if (currentItem === item) {
-            itemFound = true;
-            if (removedQuantity < quantityToRemove) {
-                user.cart.splice(i, 1);
-                removedQuantity++;
-                i--;
-            } else {
-                break;
-            }
+
+    get id(): string {
+        return this._id;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get age(): number {
+        return this._age;
+    }
+
+    get cart(): Item[] {
+        return this._cart;
+    }
+
+
+    set name(name: string) {
+        this._name = name;
+    }
+
+    set age(age: number) {
+        this._age = age;
+    }
+
+    set cart(cart: Item[]) {
+        this._cart = cart;
+    }
+
+
+    addToCart(item: Item): void {
+        this.cart.push(item);
+    }
+
+
+    removeFromCart(item: Item): void {
+        if (this.cart.includes(item)) {
+            this.cart = this.cart.filter((cartItem) => cartItem !== item);
+        } else {
+            console.log(`Item ${item.name} not in the cart!`);
         }
     }
 
-    if (!itemFound) {
-        console.log(`Error: The item (${item.name}) is not in the cart.`);
-    } else if (removedQuantity < quantityToRemove) {
-        console.log(`Error: Quantity to remove (${quantityToRemove}) exceeds the quantity of ${item.name} in the cart.`);
+    removeQuantityFromCart(item: Item, quantityToRemove: number): void {
+        let removedQuantity = 0;
+        let itemFound = false;
+
+        for (let i = 0; i < this.cart.length; i++) {
+            const currentItem = this.cart[i];
+            if (currentItem === item) {
+                itemFound = true;
+                if (removedQuantity < quantityToRemove) {
+                    this.cart.splice(i, 1);
+                    removedQuantity++;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        if (!itemFound) {
+            console.log(`Error: The item (${item.name}) is not in the cart.`);
+        } else if (removedQuantity < quantityToRemove) {
+            console.log(
+                `Error: Quantity to remove (${quantityToRemove}) exceeds the quantity of ${item.name} in the cart.`
+            );
+        }
+    }
+
+    cartTotal(): number {
+        let totalPrice = 0;
+        for (let item of this.cart) {
+            totalPrice += item.price;
+        }
+        return totalPrice;
+    }
+
+    printCart(): void {
+        console.log("Items in the cart:");
+        for (let item of this.cart) {
+            console.log(item);
+        }
     }
 }
 
 
+class Shop {
+    private _name: string;
+    private _items: Item[];
 
+    constructor(name: string, items: Item[] = []) {
+        this._name = name;
+        
+        const itemA = new Item("Item A", 10, "Description of Item A");
+        const itemB = new Item("Item B", 20, "Description of Item B");
+        const itemC = new Item("Item C", 15, "Description of Item C");
 
-function cartTotal(user:User): number {
-    let totalPrice = 0;
-    for (let item of user.cart) {
-        totalPrice += item.price;
+        this._items = [itemA, itemB, itemC, ...items];
     }
-    return totalPrice;
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(name: string) {
+        this._name = name;
+    }
+
+    get items(): Item[] {
+        return this._items;
+    }
+
+    set items(items: Item[]) {
+        this._items = items;
+    }
+
+    describe(): void {
+        console.log(`Shop Name: ${this._name}`);
+        console.log("Items in the shop:");
+        this._items.forEach(item => {
+            console.log(`- ${item.name}: $${item.price}`);
+        });
+    }
 }
 
-function printCart(user:User): void {
-    for (let item of user.cart) {
-        console.log(item);
-    };
-}
 
-// Example usage
-// Create a User
-const user = createUser("John Doe", 30);
+const shop = new Shop("My Shop");
 
-// Create Items
-const itemA = createItem("Item A", 10, "Description of Item A");
-const itemB = createItem("Item B", 20, "Description of Item B");
-const itemC = createItem("Item C", 15, "Description of Item C");
+const user = new User("John Doe", 30);
 
-// Add Item A to the user's cart
-addToCart(user, itemA);
+user.addToCart(shop.items[0]); 
+user.addToCart(shop.items[1]); 
+user.addToCart(shop.items[1]); 
+user.addToCart(shop.items[2]); 
 
-// Print the contents of the user's cart
-printCart(user);
+user.printCart();
 
-// Print the total of the user's cart
-console.log("Total cart value: $" + cartTotal(user));
 
-// Create 3 Item B and add them to the user's cart
-for (let i = 0; i < 3; i++) {
-    addToCart(user, itemB);
-}
+user.removeFromCart(shop.items[1]);
 
-// Print the contents of the user's cart
-console.log("\nAfter adding 3 Item B to the user's cart:");
-printCart(user);
+user.printCart();
 
-// Print the total of the user's cart
-console.log("Total cart value: $" + cartTotal(user));
+user.removeQuantityFromCart(shop.items[2], 1); 
 
-// Create 3 Item C and add them to the user's cart
-for (let i = 0; i < 3; i++) {
-    addToCart(user, itemC);
-}
+user.printCart();
 
-// Print the contents of the user's cart
-console.log("\nAfter adding 3 Item C to the user's cart:");
-printCart(user);
-
-// Print the total of the user's cart
-console.log("Total cart value: $" + cartTotal(user));
-
-// Remove all of Item B from the user's cart
-removeFromCart(user, itemB)
-
-// Print the contents of the user's cart
-console.log("\nAfter removing all of Item B from the user's cart:");
-printCart(user);
-
-// Print the total of the user's cart
-console.log(`Total cart value: $${cartTotal(user)}`);
-
-// Remove 2 of Item C from the user's cart using the removeQuantityFromCart function
-removeQuantityFromCart(user, itemC, 2);
-
-// Print the contents of the user's cart
-console.log("\nAfter removing 2 of Item C from the user's cart:");
-printCart(user);
-
-// Print the total of the user's cart
-console.log(`Total cart value: $${cartTotal(user)}`);
+console.log("Cart total:", user.cartTotal());
